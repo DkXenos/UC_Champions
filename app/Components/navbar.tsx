ddd835febc773ba7f2c379ade568457e188f852b
DkXenos <superjason101010@gmail.com>
2025-09-12 19:07:33 +0700

    add logo

--- app/Components/navbar.tsx
+++ app/Components/navbar.tsx
@@ -16,10 +16,11 @@ export default function Navbar() {
     <nav className="bg-white shadow-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
-          <div className="flex items-center">
-            <Link href="/">
+          <div className="flex items-center flex-row">
+            <Link href="/" className="flex items-center space-x-3">
+              <img src="/Asset/Logo/LOGO.webp" alt="UC Champions Logo" className="h-12" />
               <h1 className="text-2xl font-bold text-indigo-600">
-                UC Champions
+              UC Champions
               </h1>
             </Link>
           </div>
