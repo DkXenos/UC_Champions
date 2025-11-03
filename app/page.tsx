3e0698d467cf972d323c7e0ec22339ce082dedf6
DkXenos <superjason101010@gmail.com>
2025-11-03 07:59:12 +0700

    homepage fully responsive

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
             <iframe
               width="560"
               height="315"
-              src="https://www.youtube.com/embed/TXo3j1jmKlQ?si=fnflcHY8HbpbesEO"
+              src="https://www.youtube.com/embed/cvoRaD5Zamo"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
