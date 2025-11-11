a527f0879cbcfaf5d57aed394cf43e7138b564d0
DkXenos <superjason101010@gmail.com>
2025-11-11 14:22:01 +0700

    rework comp detail

--- app/CompetitionDetail/page.tsx
+++ app/CompetitionDetail/page.tsx
@@ -294,13 +294,13 @@ function CompetitionDetailContent() {
                     ))}
                   </div>
                 </div>
-                <div className="cp-info">
+                {/* <div className="cp-info">
                   <img
                     src="Asset/CompetitionDetail/cpbgnew.png"
                     alt="contact person background"
                     className="cpbg"
                   />
-                </div>
+                </div> */}
               </div>
 
               {/* Prizes */}
