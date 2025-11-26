090a1a7120ba00a6e6196df85af6e3444ea66540
DkXenos <superjason101010@gmail.com>
2025-11-26 22:29:06 +0700

    optimize yey

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -49,7 +49,7 @@ export default function About() {
       <div
         className="about-background"
         style={{
-          backgroundImage: `url('/Asset/Aboutpage/mainbgabout.png')`,
+          backgroundImage: `url('/Asset/Aboutpage/mainbgabout.webp')`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
